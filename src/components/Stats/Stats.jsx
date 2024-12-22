0ba787787d66fed17cd5edc59252b8bff58cc778
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-22 19:00:17 +0600

    stat section done

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,28 @@
+const Stats = () => {
+    return (
+        <div className="my-10 grid md:grid-cols-2 lg:grid-cols-5 justify-between max-w-7xl mx-auto">
+            <div className="text-center">
+                <p className="font-bold text-xl">32,000+</p>
+                <p>Experience Tutors</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold text-xl">25,000+</p>
+                <p>5-star tutor reviews</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold text-xl">120+</p>
+                <p>Subjects Taught</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold text-xl">180+</p>
+                <p>Tutor Nationalities</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold text-xl">4.8 ⭐⭐⭐⭐⭐</p>
+                <p>On the app store</p>
+            </div>
+        </div>
+    );
+};
+
+export default Stats;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import Stats from "../../components/Stats/Stats";
 
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Stats/>
         </div>
     );
 };

8054eaf2d12bf8c9cbc8c9a75b68b67b63b48553
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-22 11:56:03 +0600

    login and register page design done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,52 @@
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control">
+            <button className="btn btn-primary">Login</button>
+          </div>
+          <div className="border-2 cursor-pointer flex items-center justify-center p-2 gap-2 rounded-lg">
+                <i><FcGoogle size={25} /></i>
+                <span className="font-semibold">Login With Google</span>
+          </div>
+          <p>Do not have an account? <Link className="text-red-600" to='/register'>Register Now</Link></p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,71 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-    return (
-        <div>
-            <h1>Register page</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              name="name"
+              type="name"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span> 
+            </label>
+            <input
+              name="photo"
+              type="text"
+              placeholder="photo url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+          <p>
+            Already have an account?{" "}
+            <Link className="text-red-600" to="/login">
+              Login Now
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;

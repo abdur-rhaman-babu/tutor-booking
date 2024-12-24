4d200485fae2836c7cb0d8d6b8c610cae08782d7
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-24 10:33:59 +0600

    token remove

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -11,7 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../../Firebase/firebase.config";
-
+import axios from "axios";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -29,19 +29,19 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  const userUpdateProfile = (profile) =>{
-    return updateProfile(auth.currentUser, profile)
-  }
+  const userUpdateProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
 
-  const signOutUser = ()=>{
-    setLoading(true)
-    return signOut(auth)
-  }
+  const signOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-  const signInWithGoogle = () =>{
-    setLoading(true)
-    return signInWithPopup(auth, googleProvider)
-  }
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const authInfo = {
     createUser,
@@ -54,19 +54,33 @@ const AuthProvider = ({ children }) => {
     user,
     setUser,
     userUpdateProfile,
-    signInWithGoogle
+    signInWithGoogle,
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
       setUser(currentuser);
+      console.log("state captured", currentuser?.email);
+      if (currentuser?.email) {
+        const user = { email: currentuser?.email };
+
+        axios
+          .post("http://localhost:5000/jwt", user, { withCredentials: true })
+          .then((res) => console.log(res.data));
+      }else{
+          axios.post('http://localhost:5000/logout',{},{
+            withCredentials:true
+          })
+          .then(res=>{
+            console.log('logout', res.data)
+          })
+      }
       setLoading(false);
     });
 
     return () => {
       unsubscribe();
     };
-
   }, []);
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
